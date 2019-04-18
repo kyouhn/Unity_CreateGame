174b0ad89359ecb744722250987e7873a8cd0ad8
kyouhn <kzk0802kazuki@gmail.com>
2019-04-18 20:49:51 +0900

    矢をよけるゲーム

---
+++ CatEscape/Assets/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void LButtonDown()
+    {
+        transform.Translate(-3, 0, 0);  //左に3動かす
+    }
+
+    public void RButtonDown()
+    {
+        transform.Translate(3, 0, 0);  //左に3動かす
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        //左矢印が押されたとき
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.Translate(-3, 0, 0);  //左に3動かす
+        }
+
+        //右矢印が押されたとき
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.Translate(3, 0, 0);   //右に3動かす
+        }
+        */
+    }
+}
