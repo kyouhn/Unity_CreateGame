78410976b0dd688ae1fc4afa668576a1fab91cd2
kyouhn <kzk0802kazuki@gmail.com>
2019-05-06 00:03:15 +0900

    簡単な3Dゲームを作ってみた

---
+++ Igaguri/Assets/IgaguriController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IgaguriController : MonoBehaviour
+{
+    //いがぐりを飛ばすためのスクリプト
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    //いがぐりを的に刺す
+    void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+        GetComponent<ParticleSystem>().Play();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Shoot(new Vector3(0, 200, 2000));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
